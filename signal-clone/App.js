ef78251e549dae70410c35e8eb4c026217bdda0a
ntaztonny <atozpp@yahoo.com>
2022-04-14 00:01:12 +0200

    added dependency packages

--- signal-clone/App.js
+++ signal-clone/App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>This is a signal clone!!</Text>
+        <StatusBar style="white" />
+      </View>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ signal-clone/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDGXbna2EZnO-KMjsfWRByTYVfuuNmXHYQ",
+  authDomain: "signal-clone-cfbc2.firebaseapp.com",
+  projectId: "signal-clone-cfbc2",
+  storageBucket: "signal-clone-cfbc2.appspot.com",
+  messagingSenderId: "479751784373",
+  appId: "1:479751784373:web:74402f14e7a054fe32fbfc",
+};
