70b09d8a9f61b97dcd520f6730f453359db94460
ntaztonny <atozpp@yahoo.com>
2022-04-18 17:24:16 +0200

    Login screen update

--- signal-clone/screens/LoginScreen.js
+++ signal-clone/screens/LoginScreen.js
@@ -1,9 +1,15 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
 import { Button, Input, Image } from "react-native-elements";
 import { StatusBar } from "expo-status-bar";
 
 const LoginScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = ()=>{
+      
+  }
   return (
     <View>
       <StatusBar style="inverted" />
@@ -13,8 +19,31 @@ const LoginScreen = () => {
           uri: "https://blog.mozilla.org/internetcitizen/files/2018/08/signal-logo.png",
         }}
       />
+      <View style={styles.inputContainer}>
+        <Input
+          placeholder="Email"
+          autoFocus
+          type="Email"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <Input
+          placeholder="Password"
+          type="password"
+          value={password}
+          secureTextEntry
+          onChangeText={(text) => setPassword(text)}
+        />
+      </View>
+      <Button onPress = {signIn} containerStyle={styles.button} title="login" />
+      <Button containerStyle={styles.button} type="outline" title="Register" />
     </View>
   );
 };
 
 export default LoginScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {},
+  button: {},
+});
